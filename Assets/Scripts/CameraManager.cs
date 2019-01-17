231476b8b883991bf87a935a9f278902c661b427
Roman Ivanicky <ivanickij@gmail.com>
2019-01-17 11:04:23 +0700

    web texture fix attempt

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -6,47 +6,35 @@ using UnityEngine;
 public static class CameraManager
 {
     private static List<Texture2D> _snapshots = new List<Texture2D>();
-    private static WebCamTexture _webCamTexture;
+
+    public static WebCamTexture webCamTexture;
 
     public static event Action OnCameraStart;
     public static event Action<List<Texture2D>> OnCameraComplete;
 
     public static void StartCamera(RawImage image)
     {
-        // if (_webCamTexture == null)
-        // {
-        //     _webCamTexture = new WebCamTexture(Screen.width, Screen.height);
-        // }
+        if (webCamTexture == null)
+        {
+            webCamTexture = new WebCamTexture(Screen.width, Screen.height);
+        }
 
-        // NativeCamera.TakePicture(new NativeCamera.CameraCallback());
-        
-        
-        // _webCamTexture.Play();
-        // image.texture = _webCamTexture;
-        //
-        // _snapshots.Clear();
+        webCamTexture.Play();
+        image.texture = webCamTexture;
+
+        _snapshots.Clear();
 
         OnCameraStart?.Invoke();
     }
 
     public static Texture2D TakePhoto()
     {
-        Texture2D snapshot = new Texture2D(_webCamTexture.width, _webCamTexture.height);
+        Texture2D snapshot = new Texture2D(webCamTexture.width, webCamTexture.height);
         // Texture2D snapshot = new Texture2D(_webCamTexture.width, _webCamTexture.height);
-        snapshot.SetPixels(_webCamTexture.GetPixels());
-        Debug.Log($"Photo taken. Width: {_webCamTexture.width}, Height: {_webCamTexture.height}, req w: {_webCamTexture.requestedWidth}, req h: {_webCamTexture.requestedHeight}, rotated? {_webCamTexture.videoRotationAngle}, flipped? {_webCamTexture.videoVerticallyMirrored}");
-        
-        /*
-        float physical = (float)_webCamTexture.width/(float)_webCamTexture.height;
-        rawImageARF.aspectRatio = physical;
+        snapshot.SetPixels(webCamTexture.GetPixels());
+        Debug.Log(
+            $"Photo taken. Width: {webCamTexture.width}, Height: {webCamTexture.height}, req w: {webCamTexture.requestedWidth}, req h: {webCamTexture.requestedHeight}, rotated? {webCamTexture.videoRotationAngle}, flipped? {webCamTexture.videoVerticallyMirrored}");
 
-        float scaleY = wct.videoVerticallyMirrored ? -1f : 1f;
-        rawImageRT.localScale = new Vector3(1f, scaleY, 1f);
-
-        int orient = -wct.videoRotationAngle;
-        rawImageRT.localEulerAngles = new Vector3(0f,0f,orient);
-        */
-        
         snapshot.Apply();
 
         _snapshots.Add(snapshot);
@@ -66,11 +54,11 @@ public static class CameraManager
 
     public static void StopCamera()
     {
-        _webCamTexture.Stop();
-        
+        webCamTexture.Stop();
+
         // clear the memory
         Resources.UnloadUnusedAssets();
-     
+
         OnCameraComplete?.Invoke(_snapshots);
     }
 }
--- Assets/_Project/Scripts/UI/Screens/CameraScreen.cs
+++ Assets/_Project/Scripts/UI/Screens/CameraScreen.cs
@@ -5,19 +5,68 @@ using UnityEngine.UI;
 public class CameraScreen : MonoBehaviour
 {
     [SerializeField] protected GameObject _takePhotoContent, _managePhotoContent;
-    [SerializeField] protected RawImage _rawImage;
+    [SerializeField] protected RawImage _cameraRawImage;
     [SerializeField] protected RawImage _snapshotImage;
     [SerializeField] protected AudioClip _shutterClip;
+    [SerializeField] protected RectTransform _rawImageRT;
+    [SerializeField] protected RectTransform _parentRT;
 
     public void StartCamera()
     {
-        CameraManager.StartCamera(_rawImage);   
+        CameraManager.StartCamera(_cameraRawImage);
+
+        // change as user rotates iPhone or Android:
+        WebCamTexture wct = CameraManager.webCamTexture;
+
+        // implementation by
+        // https://answers.unity.com/questions/773464/webcamtexture-correct-resolution-and-ratio.html?childToView=1148424#answer-1148424
+
+        int cwNeeded = wct.videoRotationAngle;
+        bool willBeRotated = cwNeeded != 0;
         
+        // Unity helpfully returns the _clockwise_ twist needed
+        // guess nobody at Unity noticed their product works in counterclockwise:
+        int ccwNeeded = -cwNeeded;
+
+        // IF the image needs to be mirrored, it seems that it
+        // ALSO needs to be spun. Strange: but true.
+        if (wct.videoVerticallyMirrored) ccwNeeded += 180;
+
+        // you'll be using a UI RawImage, so simply spin the RectTransform
+        _rawImageRT.localEulerAngles = new Vector3(0f, 0f, ccwNeeded);
+
+        float videoRatio = (float) wct.width / (float) wct.height;
+
+        if (willBeRotated)
+        {
+            _rawImageRT.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _parentRT.rect.height);
+            _rawImageRT.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _parentRT.rect.width);
+        }
+        else
+        {
+            _rawImageRT.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _parentRT.rect.width);
+            _rawImageRT.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _parentRT.rect.height);
+        }
+        
+        // you'll be using an AspectRatioFitter on the Image, so simply set it
+        // rawImageARF.aspectRatio = videoRatio;
+
+        // alert, the ONLY way to mirror a RAW image, is, the uvRect.
+        // changing the scale is completely broken.
+        if (wct.videoVerticallyMirrored)
+        {
+            _cameraRawImage.uvRect = new Rect(1, 0, -1, 1); // means flip on vertical axis
+        }
+        else
+        {
+            _cameraRawImage.uvRect = new Rect(0, 0, 1, 1); // means no flip
+        }
+
         _takePhotoContent.SetActive(true);
         _managePhotoContent.SetActive(false);
         _snapshotImage.gameObject.SetActive(false);
     }
-    
+
     public void TakePhoto()
     {
         AudioManager.Instance.Play(_shutterClip);
@@ -33,7 +82,7 @@ public class CameraScreen : MonoBehaviour
     public void SavePhoto()
     {
         _snapshotImage.gameObject.SetActive(false);
-        
+
         _takePhotoContent.SetActive(true);
         _managePhotoContent.SetActive(false);
     }
@@ -42,7 +91,7 @@ public class CameraScreen : MonoBehaviour
     {
         CameraManager.RemoveLastSnapshot();
         _snapshotImage.gameObject.SetActive(false);
-        
+
         _takePhotoContent.SetActive(true);
         _managePhotoContent.SetActive(false);
     }
@@ -50,10 +99,10 @@ public class CameraScreen : MonoBehaviour
     public void Finish()
     {
         _snapshotImage.gameObject.SetActive(false);
-        
+
         // show previous screen
         ScreenManager.Instance.Back();
-        
+
         CameraManager.StopCamera();
     }
 }
