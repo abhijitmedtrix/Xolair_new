f944f492b7c31be272147824e468f70a9d25c819
Roman Ivanicky <ivanickij@gmail.com>
2019-01-18 16:42:01 +0700

    DatePicker and Calendar assets added

---
+++ Assets/GemMine/DatePicker/Scripts/WeekDayLayout.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Globalization;
+
+public class WeekDayLayout : DatePickerLayout {
+
+
+	public override void setupCells(DateTime now) {
+		// get all child objects
+		getCells ();
+
+		CultureInfo culture = CultureInfo.CurrentCulture;
+		int i = 0;
+		foreach (Button go in CalendarCells) {
+			go.transform.Find ("Text").GetComponent<Text> ().text = culture.DateTimeFormat.AbbreviatedDayNames[i].ToString ();
+			go.transform.Find ("Text").GetComponent<Text> ().fontSize = 22;
+			go.transform.Find ("Text").GetComponent<Text> ().color = fontActiveColor;
+			go.transform.GetComponent<Image> ().sprite = actualEntryImg;
+			i++;
+		}
+	}
+	
+}
